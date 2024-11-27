96bbded271c353a56b2e77ae8d6ef054abf12701
Margaux <margaux.espinasse@gmail.com>
2024-11-27 16:39:33 +0100

    nav bar implementation with logout function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
 import { ToastComponent } from './toast/toast.component';
+import { CommonModule } from '@angular/common';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ToastComponent],
+  imports: [RouterOutlet, ToastComponent, CommonModule, NavigationBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'invoice-app-client';
+  showNavbar = false;
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.showNavbar = event.urlAfterRedirects !== '/welcome';
+      }
+    });
+  }
 }
--- src/app/navigation-bar/navigation-bar.component.ts
+++ src/app/navigation-bar/navigation-bar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FetchApiDataService } from '../services/fetch-api-data.service';
+import { Router } from '@angular/router';
+import { ToastService } from '../services/toast.service';
 
 @Component({
   selector: 'app-navigation-bar',
@@ -9,4 +10,15 @@ import { FetchApiDataService } from '../services/fetch-api-data.service';
   templateUrl: './navigation-bar.component.html',
   styleUrl: './navigation-bar.component.scss',
 })
-export class NavigationBarComponent {}
+export class NavigationBarComponent {
+  constructor(private router: Router, private toastService: ToastService) {}
+
+  ngOnInit(): void {}
+
+  logoutUser(): void {
+    localStorage.clear();
+
+    this.toastService.showToast('Logged out successfully!');
+    this.router.navigate(['welcome']);
+  }
+}
