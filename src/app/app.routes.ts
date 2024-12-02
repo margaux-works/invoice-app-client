f233c3511d85dcd0601203fbec2d194dbd41ffc9
Margaux <margaux.espinasse@gmail.com>
2024-12-02 13:07:46 +0100

    invoicedetails route added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserLoginFormComponent } from './login/login.component';
-import { InvoiceDetailsComponent } from './invoice-details/invoice-details.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { InvoiceDetailsComponent } from './invoice-details/invoice-details.component';
 
 export const routes: Routes = [
   { path: 'welcome', component: WelcomePageComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'login', component: UserLoginFormComponent },
   { path: 'invoice/:id', component: InvoiceDetailsComponent },
+
   { path: '', redirectTo: 'welcome', pathMatch: 'full' },
 ];
