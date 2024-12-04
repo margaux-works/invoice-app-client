6c585b5ae3a549ff6d969b437d338ad6c265e2dc
Margaux <margaux.espinasse@gmail.com>
2024-12-04 15:04:35 +0100

    dashboard styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,35 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: {
+          light: "#9277FF",
+          DEFAULT: "#7c5dfa",
+        },
+        red: {
+          light: "#FF9797",
+          DEFAULT: "#EC5757",
+        },
+        gray: {
+          DEFAULT: "#888EB0",
+        },
+        green: {
+          light: "#33D69F0F",
+          DEFAULT: "#33D69F",
+        },
+        orange: {
+          light: "#FF8F000F",
+          DEFAULT: "#FF8F00",
+        },
+        blue: {
+          light: "##373B530F",
+          DEFAULT: "##373B53",
+        },
+        lightBg: "#F8F8FB",
+        darkBg: "#141625",
+      },
+    },
   },
   plugins: [],
 };
